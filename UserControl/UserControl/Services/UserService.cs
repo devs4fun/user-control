8f9fed2cbb04149aa5d36b700e57c5c9c041d89a
Lucas Nobre <lucasnobredev@outlook.com>
2018-12-13 23:57:08 -0200

    initial commit (#3)

---
+++ UserControl/UserControl/Services/UserService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UserControl.Models;
+
+namespace UserControl.Services
+{
+    public class UserService
+    {
+        public User CreateUser()
+        {
+            return new User();
+        }
+
+        public User GetById(int id)
+        {
+            // algum código
+
+            return new User();
+        }
+    }
+}
