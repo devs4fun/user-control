2ea3b38151465b8caac492efd1bfc4663202f0f7
Lucas Nobre <lucasnobredev@outlook.com>
2018-12-13 23:48:43 -0200

    initial commit

---
+++ UserControl/UserControl/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UserControl.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Profile> Profiles { get; set; }
+    }
+}
